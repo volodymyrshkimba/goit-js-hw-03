45174ec12833c955d34127562382d7dd400e32b6
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-06-19 15:49:35 +0300

    first check

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,18 @@
-function getShippingMessage(country, price, deliveryFee) {
-	return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+function makeArray(firstArray, secondArray, maxLength) {
+
+	const commonArray = firstArray.concat(secondArray);
+
+	if (commonArray.length > maxLength) {
+		return commonArray.slice(0, maxLength); 
+	} else {
+		return commonArray;
+	}
+
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
